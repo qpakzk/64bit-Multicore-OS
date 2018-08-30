e284f38afe1878c7361aa73c4f0ccebfd38d4f05
Sangwon Hong <qpakzk@gmail.com>
2018-08-30 20:17:35 +0900

    00.BootLoader: implement a bootloader to print a msg

--- 00.BootLoader/BootLoader.asm
+++ 00.BootLoader/BootLoader.asm
@@ -3,13 +3,46 @@
 
 SECTION .text
 
-mov ax, 0xB800
-mov ds, ax
+jmp 0x07C0:START
 
-mov byte [ 0x00 ], 'M'
-mov byte [ 0x01 ], 0x4A
+START:
+    mov ax, 0x07C0
+    mov ds, ax
 
-jmp $
+    mov ax, 0xB800
+    mov es, ax
+
+    mov si, 0
+
+.SCREENCLEARLOOP:
+    mov byte [ es: si ], 0
+    mov byte [ es: si + 1 ], 0x0A
+
+    add si, 2
+
+    cmp si, 80 * 25 * 2
+    jl .SCREENCLEARLOOP
+
+    mov si, 0
+    mov di, 0
+
+.MESSAGELOOP:
+    mov cl, [ MESSAGE1 + si ]
+    
+    cmp cl, 0
+    je .MESSAGEEND
+
+    mov [ es: di ], cl
+
+    add si, 1
+    add di, 2
+
+    jmp .MESSAGELOOP
+.MESSAGEEND:
+
+    jmp $
+
+MESSAGE1: db 'Wonix Boot Loader Start~!!', 0
 
 times 510 - ($ - $$) db 0x00
 
